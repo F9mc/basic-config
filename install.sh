16dd06b3caae614711dc10e6d7134e03cc74dae1
Paul <paul.cabon@protonmail.com>
2023-01-31 16:26:47 +0100

    add script for workstations #4

--- install.sh
+++ install.sh
@@ -2,12 +2,21 @@
 
 ## Install Packages
 # Debian
-sudo apt install bash exa curl git -y
+sudo apt install bash exa curl git python3 -y
 
 ## Git clone
+cd /tmp
 git clone https://github.com/Efrei-Paul/basic-config.git
-cd basic-config
+cd /tmp/basic-config
 
+while getops 'l:' OPTION; do
+    case "$OPTION" in
+        a)
+        echo `\nInstalling Workstation packages\n`
+        bash ./install-workstation.sh
+        ;;
+    esac
+done
 ## Set Up
 cp ./bashrc ~/.bashrc
 sudo cp ./bashrc ~/.bashrc
