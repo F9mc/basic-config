294f1dcd3cae186d41cdd4e2d4d945f6e0acd875
Paul <paul.cabon@protonmail.com>
2023-01-31 16:37:51 +0100

    redirect stderr & stdout

--- install.sh
+++ install.sh
@@ -2,23 +2,23 @@
 
 ## Install Packages
 # Debian
-sudo apt install bash exa curl git python3 -y
+sudo apt install bash exa curl git python3 -y &> /dev/null  
 
 ## Git clone
 cd /tmp
-git clone https://github.com/Efrei-Paul/basic-config.git
+git clone https://github.com/Efrei-Paul/basic-config.git 
 cd /tmp/basic-config
 
 while getops 'ad:' OPTION; do
     case "$OPTION" in
         a)
         echo `\nInstalling Workstation packages\n`
-        bash ./lib/install-workstation.sh
+        bash ./lib/install-workstation.sh &> /dev/null  
         ;;
         d)
         echo `\nInstalling Docker\n`
         curl -fsSL https://get.docker.com -o get-docker.sh
-        sudo sh get-docker.sh
+        sudo sh get-docker.sh &> /dev/null  
         echo "Adding user $USER to docker group"
         sudo usermod -aG docker $USER
     esac
