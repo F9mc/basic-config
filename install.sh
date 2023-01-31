333f205003aee698f045c0fb1af6d9849b873a5a
Paul <paul.cabon@protonmail.com>
2023-01-31 16:32:43 +0100

    add docker install script

--- install.sh
+++ install.sh
@@ -9,12 +9,18 @@ cd /tmp
 git clone https://github.com/Efrei-Paul/basic-config.git
 cd /tmp/basic-config
 
-while getops 'l:' OPTION; do
+while getops 'ad:' OPTION; do
     case "$OPTION" in
         a)
         echo `\nInstalling Workstation packages\n`
         bash ./lib/install-workstation.sh
         ;;
+        d)
+        echo `\nInstalling Docker\n`
+        curl -fsSL https://get.docker.com -o get-docker.sh
+        sudo sh get-docker.sh
+        echo "Adding user $USER to docker group"
+        sudo usermod -aG docker $USER
     esac
 done
 ## Set Up
