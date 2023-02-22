5c6c9184bcc60f6f2c1c2d6e3004495a6bb40af1
F9mc <F9mc.local>
2023-02-22 14:57:17 +0100

    install script

--- install.sh
+++ install.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 
-## Install Packages
-# Debian
-sudo apt install bash exa curl git python3 -y &> /dev/null  
-
-## Git clone
-cd /tmp
-git clone https://github.com/F9mc/basic-config.git 
-cd /tmp/basic-config
-
-while getops 'ad:' OPTION; do
-    case "$OPTION" in
-        a)
-        echo `\nInstalling Workstation packages\n`
-        bash ./lib/install-workstation.sh &> /dev/null  
-        ;;
-        d)
-        echo `\nInstalling Docker\n`
-        curl -fsSL https://get.docker.com -o get-docker.sh
-        sudo sh get-docker.sh &> /dev/null  
-        echo "Adding user $USER to docker group"
-        sudo usermod -aG docker $USER
-    esac
-done
-## Set Up
-cp ./dotfiles/bashrc ~/.bashrc
-sudo cp ./dotfiles/bashrc ~/.bashrc
+cp -r dotfiles/.cargo ~/
+cp dotfiles/.vimrc ~/
+cp dotfiles/.bashrc ~/
+cp dotfiles/.gitconfig ~/
